7ce942d6b60a20fbfa194f1451224e84c1c146d3
Karlo Andric <karloandric81@gmail.com>
2024-03-05 10:01:59 +0100

    corrected card urls

--- src/constants/imageData.ts
+++ src/constants/imageData.ts
@@ -1,13 +1,14 @@
 const cardImages: string[] = [
-    '/src/assets/card-images/card-image1.png',
-    '/src/assets/card-images/card-image2.png',
-    '/src/assets/card-images/card-image3.png',
-    '/src/assets/card-images/card-image4.png',
-    '/src/assets/card-images/card-image5.png',
-    '/src/assets/card-images/card-image6.png',
-    '/src/assets/card-images/card-image7.png',
-    '/src/assets/card-images/card-image8.png',
-    '/src/assets/card-images/card-image9.png',
+    './src/assets/card-images/card-image1.png',
+    './src/assets/card-images/card-image2.png',
+    './src/assets/card-images/card-image3.png',
+    './src/assets/card-images/card-image4.png',
+    './src/assets/card-images/card-image5.png',
+    './src/assets/card-images/card-image6.png',
+    './src/assets/card-images/card-image7.png',
+    './src/assets/card-images/card-image8.png',
+    './src/assets/card-images/card-image9.png',
   ];
   
   export default cardImages;
+  
