dec8fba85d0cefc4b6d7e3a98982f1f5155e7713
Amira Kunalic <amirakunalic@gmail.com>
2024-03-21 14:41:24 +0100

    BrowserRouter replaced with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/index.tsx";
-import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import App from './components/App/index.tsx';
+import './index.scss';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
