6a0edfd6e2f3f40702ae93e90bd9d5a0350217b7
Karlo Andric <karloandric81@gmail.com>
2024-03-21 14:39:14 +0100

    removed unnecessary code

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Home";
 import GoLive from "../GoLive";
 import Login from "../Login";
+import { FilterProvider } from "../../contexts/FilterContext";
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/go-live" element={<GoLive />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
+  <FilterProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/go-live" element={<GoLive />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </FilterProvider>
 );
 
 export default App;
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,11 @@
-import { FilterProvider } from "../../contexts/FilterContext";
 import HeaderMinimal from "../HeaderMinimal";
 import LoginBody from "../LoginBody";
 
 const Login = () => (
-  <FilterProvider>
+  <>
     <HeaderMinimal />
     <LoginBody />
-  </FilterProvider>
+  </>
 );
 
 export default Login;
