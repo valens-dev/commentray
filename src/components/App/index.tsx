cbaf419a57ea298d4f178b784b30a8e4f852952b
Karlo Andric <karloandric81@gmail.com>
2024-03-22 10:23:13 +0100

    added 2nd login page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Home";
 import GoLive from "../GoLive";
 import Login from "../Login";
+import Login2 from "../Login2";
 import { FilterProvider } from "../../contexts/FilterContext";
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
       <Route path="/" element={<Home />} />
       <Route path="/go-live" element={<GoLive />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/login2" element={<Login2 />} />
     </Routes>
   </FilterProvider>
 );
---
+++ src/components/Login2/index.tsx
@@ -0,0 +1,11 @@
+import HeaderMinimal from "../HeaderMinimal";
+import LoginBody2 from "../LoginBody2";
+
+const Login2 = () => (
+  <>
+    <HeaderMinimal />
+    <LoginBody2 />
+  </>
+);
+
+export default Login2;
